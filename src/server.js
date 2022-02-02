1da2731aed823f44b3817cd61115e685b75c5aa8
Vitor <vitormuller66@gmail.com>
2022-02-02 20:31:04 -0300

    feat: user resgistration ok

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 import joi from "joi"
 import dayjs from 'dayjs'
+import bcrypt from "bcrypt"
 
 
 
@@ -12,6 +13,12 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+const userSignUpModel = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    password: joi.string().required()
+})
 
 let db;
 
@@ -23,3 +30,28 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 mongoClient.connect().then(() => {
     db = mongoClient.db("api-my-wallet")
 });
+
+app.post('/signin', (req, res) => {
+    const user = req.body
+    res.send(200)
+
+})
+
+app.post('/signup', async (req, res) => {
+    const user = req.body
+    const passwordHash = bcrypt.hashSync(user.password, 10)
+
+
+
+    await db.collection("users").insertOne({ ...user, password: passwordHash })
+
+
+
+})
+
+app.get("/users", async (req, res) => {
+    const users = await db.collection("users").find({}).toArray()
+
+    res.send(users)
+
+})
